b5f3823068b6922197b9659187a6c117b323de89
Елена Василькова <vasilkovaea@gmail.com>
2023-03-09 12:51:13 +0300

    Add tests

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -10,7 +10,19 @@ class SignUp(CreateView):
     success_url = reverse_lazy('posts:index')
     template_name = 'users/signup.html'
 
+    def get_context_data(self):
+        context = {
+            'title': 'Зарегистрироваться'
+        }
+        return context
+
 
 class PasswordChange(PasswordChangeView):
     success_url = 'done/'
     template_name = 'user/password_change_done.html'
+
+    def get_context_data(self):
+        context = {
+            'title': 'Сброс пароля'
+        }
+        return context
